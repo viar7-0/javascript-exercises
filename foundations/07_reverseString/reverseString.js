23ad0b3a07a5891d89a27e912b8296ca65a11834
viar7-0 <vitiarora7@gmail.com>
2026-06-09 23:18:04 +0530

    Add function to reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let ans=''
+    for (let i=word.length-1;i>=0;i--){
+        ans+=word[i]
+    }
+    return ans
 
 };
 
