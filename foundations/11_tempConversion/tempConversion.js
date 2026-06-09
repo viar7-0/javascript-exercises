c8b34c60965cd34bb3be6050e72790fd5bf64f0d
viar7-0 <vitiarora7@gmail.com>
2026-06-10 00:28:13 +0530

    Add temperature conversion functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  cTemp = (temp-32)*(5/9)
+  let rounded = Math.round(cTemp*10)/10
+  return rounded
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fTemp = (temp*(9/5)) +32
+  let rounded = Math.round(fTemp*10)/10
+  return rounded
 };
 
 // Do not edit below this line
