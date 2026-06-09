cb66448db204f18a15204d81831fb83b56a55a8c
viar7-0 <vitiarora7@gmail.com>
2026-06-10 00:06:53 +0530

    Add ints in a range

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(lowerBound,highBound) {
+    if(!(Number.isInteger(lowerBound) && Number.isInteger(highBound))){
+        return "ERROR"
+    }
+    if(!(lowerBound>=0 && highBound>=0)){
+        return "ERROR"
+    }
+     if(lowerBound>highBound){
+        let q = highBound
+        highBound = lowerBound
+        lowerBound = q
+     }
+   
+    let sum =0
+    for(let i=lowerBound;i<=highBound;i++){
+        sum+=i;
+    }
+    return sum
+
 
 };
 
