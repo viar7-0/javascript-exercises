4bf2af144cae14daefb80a303441afc826734da8
viar7-0 <vitiarora7@gmail.com>
2026-06-10 12:52:24 +0530

    Add function to remove selected elements

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...extra) {
+    let ans =[]
+    for (let i=0;i<arr.length;i++){
+        if(!extra.includes(arr[i])){
+            ans.push(arr[i])
+
+        }
+    }
+    return ans
 };
 
 // Do not edit below this line
