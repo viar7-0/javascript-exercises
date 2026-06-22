19b00f2ec408141d3648f36ed05e256d15437ee7
viar7-0 <vitiarora7@gmail.com>
2026-06-22 21:38:56 +0530

    modify palindrome function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (st) {
+    let s=""
+    for(let i=0;i<st.length;i++){
+
+        a=st.at(i)
+        if(!((a>='a' && a<='z') || (a>='A' && a<='Z') || (a>='0' && a<='9'))) continue;
+        c=a.toLowerCase()
+        s+=c
+    }
+    rev=''
+    for(let i=s.length-1;i>=0;i--){
+        rev+=s.at(i)
+
+    }
+    if(rev==s) return true
+    else return false
 
 };
 
