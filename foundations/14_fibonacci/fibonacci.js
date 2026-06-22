d112bed14824abbe50d7bb901c1dbc3ea2bc685b
viar7-0 <vitiarora7@gmail.com>
2026-06-22 22:17:31 +0530

    Modify fibonacci function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    let fb =[]
+    fb[0] =1;
+    fb[1] =1
+    for(let i=2;i<num;i++){
+        fb[i] =fb[i-1] +fb[i-2]
+    }
+    if(num<0) return "OOPS"
+
+    if(num==0) return 0
+    return fb[num-1]
 
 };
 
