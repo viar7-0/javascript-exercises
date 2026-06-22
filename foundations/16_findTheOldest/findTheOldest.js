8d66030682043e3b6e88f7ebc4a39297319ae44b
viar7-0 <vitiarora7@gmail.com>
2026-06-22 22:43:49 +0530

    completed findThe Oldest()

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const person = people.sort((a,b) => {
+        adod =a.yearOfDeath
+        if(adod==undefined) {adod=(new Date()).getFullYear()}
+        bdod =b.yearOfDeath
+        if(bdod==undefined){bdod=(new Date()).getFullYear()}
+        return (adod-a.yearOfBirth)>(bdod - b.yearOfBirth) ?-1:1
+    }
+    )
+    return person[0]
 
 };
 
